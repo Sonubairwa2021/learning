c0245a61dc2f02c557a8233ef9eb709e73746bc7
sohan bairwa <sohanbairwa2021@gmail.com>
2025-11-15 11:42:27 +0530

    Add payment functionality to product listing page with Razorpay integration. Updated routes for order creation and payment verification.

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\PaymentController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-
 Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+
+// Payment routes
+Route::post('/payment/create-order', [PaymentController::class, 'createOrder'])->name('payment.create-order');
+Route::post('/payment/verify', [PaymentController::class, 'verifyPayment'])->name('payment.verify');
